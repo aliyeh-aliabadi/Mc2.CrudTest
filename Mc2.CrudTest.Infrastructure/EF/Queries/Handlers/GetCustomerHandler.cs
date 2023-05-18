7d852b107bd7cb8f2b77e115107914a884078b9f
Aliyeh <aliyeh_aliabadi@yahoo.com>
2023-05-18 13:04:42 +0330

    - Create Customer Model in Infrastructure project

---
+++ Mc2.CrudTest.Infrastructure/EF/Queries/Handlers/GetCustomerHandler.cs
@@ -0,0 +1,20 @@
+﻿using Mc2.CrudTest.Application.Dto;
+using Mc2.CrudTest.Application.Queries;
+using Mc2.CrudTest.Infrastructure.EF.Models;
+using Mc2.CrudTest.Shared.Abstractions.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Infrastructure.EF.Queries.Handlers
+{
+    internal sealed class GetCustomerHandler : IQueryHandler<GetCustomer, CustomerDto>
+    {
+        public Task<CustomerDto> HandleAsync(GetCustomer query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
