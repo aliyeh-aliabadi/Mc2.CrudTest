ed1ea6f6d04dc7f134140e1e397dc60d3c5df5e1
Aliyeh <aliyeh_aliabadi@yahoo.com>
2023-05-18 09:32:58 +0330

    - Update Customer Entity: Inherit from AggregateRoot - Update Customer Entity: Add Constructors

--- Mc2.CrudTest.Domain/Entities/Customer.cs
+++ Mc2.CrudTest.Domain/Entities/Customer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mc2.CrudTest.Domain.ValueObjects;
+using Mc2.CrudTest.Shared.Abstractions.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,32 @@ using System.Threading.Tasks;
 
 namespace Mc2.CrudTest.Domain.Entities
 {
-    internal class Customer
+    public class Customer : AggregateRoot<CustomerId>
     {
+        public CustomerId Id { get; private set; }
+        private CustomerFirstName _name;
+        private CustomerLastName _lastName;
+        private CustomerDateOfBirth _dateOfBirth;
+        private CustomerPhoneNumber _phoneNumber;
+        private CustomerEmail _email;
+        private CustomerBankAccountNumber _bankAccountNumber;
+
+        internal Customer(CustomerId id,
+           CustomerFirstName name,
+           CustomerLastName lastName,
+           CustomerDateOfBirth dateOfBirth,
+           CustomerPhoneNumber customerPhoneNumber,
+           CustomerEmail email,
+           CustomerBankAccountNumber bankAccountNumber)
+        {
+            Id = id;
+            _name = name;
+            _lastName = lastName;
+            _dateOfBirth = dateOfBirth;
+            _phoneNumber = customerPhoneNumber;
+            _email = email;
+            _bankAccountNumber = bankAccountNumber;
+        }
+        public Customer() { }
     }
 }
