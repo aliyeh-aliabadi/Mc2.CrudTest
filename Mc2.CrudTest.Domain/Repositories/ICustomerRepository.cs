00cf08ef48669cf05c1806d8781891b2e812e069
Aliyeh <aliyeh_aliabadi@yahoo.com>
2023-05-18 09:40:57 +0330

    Create ICustomerRepository

---
+++ Mc2.CrudTest.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+using Mc2.CrudTest.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.Repositories
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer> CreateAsync(CustomerId id);
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(CustomerId id);
+    }
+}
