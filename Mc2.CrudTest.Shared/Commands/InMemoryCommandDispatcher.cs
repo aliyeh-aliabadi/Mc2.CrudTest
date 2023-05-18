e5233d1c5f5c56d4ffe6babb0169f60323fa7bd9
Aliyeh <aliyeh_aliabadi@yahoo.com>
2023-05-18 17:11:21 +0330

    - Create Queries/Commands Dispatchers

---
+++ Mc2.CrudTest.Shared/Commands/InMemoryCommandDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using Mc2.CrudTest.Shared.Abstractions.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Shared.Commands
+{
+    public class InMemoryCommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InMemoryCommandDispatcher(IServiceProvider serviceProvider)
+            => _serviceProvider = serviceProvider;
+
+        public async Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, ICommand
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+            await handler.HandleAsync(command);
+        }
+    }
+}
