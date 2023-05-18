3aa7d10f318546dc75403f813337c941e7518cad
Aliyeh <aliyeh_aliabadi@yahoo.com>
2023-05-18 12:30:36 +0330

    - Add GetCustomer Query to Application layer

---
+++ Mc2.CrudTest.Application/Queries/GetCustomer.cs
@@ -0,0 +1,15 @@
+﻿using Mc2.CrudTest.Application.Dto;
+using Mc2.CrudTest.Shared.Abstractions.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Queries
+{
+    public class GetCustomer : IQuery<CustomerDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
